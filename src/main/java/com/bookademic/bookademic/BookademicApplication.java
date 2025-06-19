19246dc7edd66b0dadff3a4a40919a10a294785b
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-06-19 12:38:14 -0300

    Create proyect

---
+++ src/main/java/com/bookademic/bookademic/BookademicApplication.java
@@ -0,0 +1,13 @@
+package com.bookademic.bookademic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookademicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookademic/bookademic/BookademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookademic.bookademic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
